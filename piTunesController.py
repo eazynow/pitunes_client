db4fbb1d00863eef2926fdf486416f0f689de00f
telecoda <rob@thebaines.co.uk>
2012-12-11 14:30:15 +0000

    commit piTunesController.py

---
+++ piTunesController.py
@@ -0,0 +1,112 @@
+import soundcloud
+import subprocess
+import random
+
+print "Starting piTunes\n"
+
+class piTunesController:
+
+
+	def __init__(self):
+		print "Init piTunes controller"
+		self.client = soundcloud.Client(client_id='8da91e376c359e86e2ef2ea5f3008514')
+
+
+		self.client = soundcloud.Client(client_id='8da91e376c359e86e2ef2ea5f3008514',
+                           client_secret='7b38fab0df5b9d131d388d7118c43467',
+                           username='rob.baines@hibu.com',
+                           password='pitunes')
+		self.still_running = True
+
+
+	def announce_track(self,track):
+		pass
+
+	def call_request_api(self):
+		# return dummy request for now
+
+		request = {}
+		request['username'] = 'dj hawkins'
+		request['search'] = 'drum and bass'
+		request['message'] = 'this one is for my mum'
+
+		print request
+		return request
+
+	def filler_speech(self):
+		pass
+
+	def play_track(self,track):
+		pass
+
+	def run(self):
+		print "Starting piTunes controller"
+
+		while(self.still_running):
+
+			# get next track
+			next_track_req = self.call_request_api()
+
+			# if no track suggested
+			if(next_track_req==None):
+				self.filler_speech()
+			else:
+				# search soundcloud
+				track=self.search_soundcloud(next_track_req['search'])
+
+				if(track==None):
+					self.filler_speech()
+
+				# announce track
+				self.announce_track(track)
+				
+				# play track
+				self.play_track(track)
+
+				# filler speak
+				self.filler_speech()
+
+			# repeat forever?
+
+	def search_soundcloud(self,search):
+		# fetch track to stream
+
+		print "Searching for %s" % search
+
+		tracks = self.client.get('/tracks', q=search)
+		if(tracks==None):
+			return None
+
+		total_tracks = len(tracks)
+
+		print "Found %d" % total_tracks
+
+		random_id = int(random.random()*total_tracks)
+		print "Random_id:" + str(random_id)
+		# get the tracks streaming URL
+		track = tracks[random_id]
+
+		stream_url = self.client.get(track.stream_url, allow_redirects=False)
+
+		suggested_track = {}
+		suggested_track['stream_url']=stream_url.location
+		suggested_track['track_details'] = track
+
+		print "Suggested Track"		
+		print "==============="		
+		title = suggested_track['track_details'].title
+		description = suggested_track['track_details'].description
+
+		if title!= None:
+			print "Title:"+title
+
+		if description!= None:
+			print "Description:"+description
+	
+		return suggested_track
+	
+def main():
+    controller = piTunesController()    
+    controller.run()
+
+if  __name__ =='__main__':main()
